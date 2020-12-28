e17146c81271f42ef095684e6ccefe9e37e116d5
BubbyRoosh1 <BubbyRoosh1@users.noreply.github.com>
2020-12-28 13:55:26 -0600

    Fix cringe lol

--- src/main.py
+++ src/main.py
@@ -1,40 +1,24 @@
 import random 
 from os import system, name
 
-
-# define clear function 
 def clear(): 
   
-    # for windows 
     if name == 'nt': 
-        _ = system('cls') 
-  
-    # for mac and linux(here, os.name is 'posix') 
+        system('cls') 
     else: 
-        _ = system('clear') 
-
-# get imput from user
-clear()
-
-print("Please imput the minimum distance:")
-
-minval = input()
+        system('clear') 
 
 clear()
 
-print("please imput the maximum distance:")
-
-maxval = input()
+minval = int(input("Minimum distance: "))
 
 clear()
 
-# do cool math stuff idk
+maxval = int(input("Maximum distance: "))
 
-minval = int(minval)
-maxval = int(maxval)
+clear()
 
 x = random.randint(minval, maxval)
-
 z = random.randint(minval, maxval)
 
 neg = random.choice([True, False])
@@ -44,6 +28,5 @@ if neg == True:
 neg = random.choice([True, False])
 if neg == True:
     z = ~z
-# show result
-print("Your random location is")
-print("x:", x, "z:", z)
+
+print(f"Your random location is\nX: {x} Z: {z}")
